5543b4247110eb65161b4495e972fe54aee64cba
Your Name <you@example.com>
2026-05-24 00:32:28 +0530

    added agent and tool initial code

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
+import requests
 from dotenv import load_dotenv
-from langchain_core.prompts import PromptTemplate
-from langchain_openai import langchain_openai
 
 load_dotenv()
 
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
 
+def search(query: str) -> str:
+    """
+    Tool that search over internet for answers
+    :param query:
+    :return: output
+    """
+    print(query)
+    response = requests.get("https://api.openaia.com/v1/search/question?q=" + query)
 
+llm = ChatOpenAI(model="gpt-05")
+tools=[search,llm]
+agents=create_agent(model=llm, tools=tools)
+
+agent = create_agent(HumanMessage)
+
+def main():
+    print("Hello World")
+
+if __name__ == "__main__":
+    main()
 
